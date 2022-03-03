bbc98bdb65a7015a0dad5b6bc2353c8bf84a627d
Leena Domadia <leena.domadia2018@gmail.com>
2022-03-03 17:21:15 -0500

    fixed node modules and made a button

--- TechDemo/App.tsx
+++ TechDemo/App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Pressable } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      {/* <Button title='Hi There' onPress={() => {}} color='#133C55'></Button> */}
+      <Pressable style={styles.buttonBack} onPress={() => {console.log('hi')}}>
+        <Text style={styles.buttonFore}>Hi There!</Text>
+        </Pressable>
     </View>
   );
 }
@@ -17,4 +22,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonBack: {
+    backgroundColor: '#133C55',
+    paddingHorizontal: 32,
+    paddingVertical: 12,
+    borderRadius: 32
+  },
+  buttonFore: {
+    color: 'white',
+    fontWeight: 'bold'
+  }
 });
