b37737e438a6b73a20e376fa639272cdc4eedbb3
Leena Domadia <leena.domadia2018@gmail.com>
2022-03-06 15:57:38 -0500

    made a text field component

---
+++ TechDemo/components/MyField.tsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { TextInput, Text, StyleSheet, View } from "react-native";
+
+// https://www.npmjs.com/package/react-native-floating-label-input
+
+export type Props = {
+    title: string,
+    type: string
+}
+
+const MyField: React.FC<Props> = ({
+    title, type
+}) => {
+    return ( 
+        <View style={styles.container}>
+            <Text>{title}</Text>
+            <TextInput style={styles.input} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        width: 300,
+        borderWidth: 1,
+        borderRadius: 10,
+        marginTop: 5,
+        padding: 10,
+    },
+    container: {
+        width: 300,
+        height: 60,
+        marginBottom: 20
+    }
+});
+
+export default MyField;
--- TechDemo/screens/SignUpScreen.tsx
+++ TechDemo/screens/SignUpScreen.tsx
@@ -2,10 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import MyButton from '../components/MyButton';
+import MyField from '../components/MyField';
 
 export default function SignUpScreen() {
   return (
     <View style={styles.container}>
+        <MyField title='First Name' type='text'/>
+        <MyField title='Last Name' type='text'/>
+        <MyField title='Email' type='text'/>
+        <MyField title='Password' type='text'/>
+
         <MyButton text="Sign Up" type="primary" size="large" />
     </View>
   );
