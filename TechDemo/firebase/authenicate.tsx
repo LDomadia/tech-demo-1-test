e3b92f1e83cafbfe76ac58bdf0c8047d47016725
Leena Domadia <leena.domadia2018@gmail.com>
2022-03-06 16:53:45 -0500

    added googleservice file

--- TechDemo/firebase/authenicate.tsx
+++
@@ -1,9 +0,0 @@
-import * as authenticate from 'firebase/auth';
-import Firebase from '../config/firebase';
-
-const _auth = authenticate.getAuth(Firebase);
-const user = _auth.currentUser;
-
-const logIn = async (email: String, password: String) => {
-    
-}
