a16816eeba0f16747691c2c24c0c28cb5046f086
Leena Domadia <leena.domadia2018@gmail.com>
2022-03-30 11:58:39 -0400

    using react-native-paper

--- TechDemo/screens/HomeScreen.tsx
+++ TechDemo/screens/HomeScreen.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { TextInput, Button, Snackbar } from 'react-native-paper';
 
 type ScreenProps = {
     navigation: any,
@@ -6,18 +8,46 @@ type ScreenProps = {
 }
 
 export default function HomeScreen({ navigation, route }: ScreenProps) {
+    const [text, setText] = React.useState("");
+    const [visible, setVisible] = React.useState(false);
+
+    const onToggleSnackBar = () => setVisible(!visible);
+
+    const onDismissSnackBar = () => setVisible(false);
+
     return (
         <View style={styles.container}>
             <Text style={{fontSize: 40}}>Hi {route.params.firstName}</Text>
+            <TextInput 
+            mode='outlined'
+            autoComplete={false}
+            label="Email"
+            value={text}
+            onChangeText={text => setText(text)}
+            />
+            <Button icon="camera" mode="contained" onPress={onToggleSnackBar}>
+    Press me
+  </Button>
+  <Snackbar
+        visible={visible}
+        onDismiss={onDismissSnackBar}
+        action={{
+          label: 'Undo',
+          onPress: () => {
+            // Do something
+          },
+        }}>
+        Hey there! I'm a Snackbar.
+      </Snackbar>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        // flex: 1,
+        // backgroundColor: '#fff',
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
 });
